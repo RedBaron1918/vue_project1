dd87bb2d0252e4340f93c668016281f0821efa3f
RedBaron1918 <asatianikotiko1@gmail.com>
2022-07-03 15:56:34 +0400

    adding button and header with styles

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button :style="{background:color}">{{text}}</button>
+</template>
+
+<script>
+export default {
+props:{
+    text:String,
+    color:String
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+   <h1>{{title}}</h1>
+   <Button class="btn" text="add task" color="green"/>
+   
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+    components:{Button},
+props:
+{
+    title:{
+        type:String,
+        required:true
+    }
+},
+
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+
+
+</style>
