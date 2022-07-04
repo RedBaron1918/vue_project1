a1b8f0b024197a8ce5c261cf2611c58532839464
RedBaron1918 <asatianikotiko1@gmail.com>
2022-07-04 15:36:55 +0400

    adding stuff

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="task">
-     <h3>{{task.text}}</h3>
+     <h3>{{task.text}} <i class="fas fa-times"></i></h3>
      <p>{{task.day}}</p>
 </div>
  
@@ -12,6 +12,22 @@ props:['task']
 }
 </script>
 
-<style>
-
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
